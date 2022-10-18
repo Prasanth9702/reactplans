f0f43dee0782066863c8ee150557b6310a0952b7
prasanth <prasanthkn02@gmail.com>
2022-10-18 11:02:55 -0600

    space

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faCheck } from '@fortawesome/free-solid-svg-icons';
-function Card(pro){
+function Card({cvalue}){
     return(
         <>
         <div className="col-lg-4">
         <div className="card mb-5 mb-lg-0">
           <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center">{pro.cvalue.type}</h5>
-            <h6 className="card-price text-center">${pro.cvalue.price}<span className="period">/months</span></h6>
+            <h5 className="card-title text-muted text-uppercase text-center">{cvalue.type}</h5>
+            <h6 className="card-price text-center">${cvalue.price}<span className="period">/months</span></h6>
             <hr/>
             <ul className="fa-ul">
               {
-                pro.cvalue.feature.map((note)=>{
-                  return note.test?<div className="font-card"><FontAwesomeIcon icon={faCheck} className="icon" /> <strong>{note.name1}</strong>{note.name}</div>:<div className="font-card text-muted"><FontAwesomeIcon icon={faXmark} className="icon" /> {note.name}</div>
+              cvalue.feature.map((note)=>{
+                  return note.test?<div className="font-card"><FontAwesomeIcon icon={faCheck} /> <strong>{note.name1}</strong>{note.name}</div>:<div className="font-card text-muted"><FontAwesomeIcon icon={faXmark}  /> {note.name}</div>
                 })
               }
             </ul>
